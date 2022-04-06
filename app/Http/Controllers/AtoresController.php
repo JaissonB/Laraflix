ddeb9398fd03501b73a86a67f80618385aa34764
JaissonB <097319@aluno.uricer.edu.br>
2022-04-06 18:29:01 -0300

    First commit

---
+++ app/Http/Controllers/AtoresController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AtoresController extends Controller
+{
+    public function index () {
+        $nome = "Johnny Depp";
+        return view('atores', ['nome'=>$nome]);
+    }
+}
