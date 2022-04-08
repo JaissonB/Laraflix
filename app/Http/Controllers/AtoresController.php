0e33d0cd5af2f7222220c1437b5fb3d523db7602
JaissonB <097319@aluno.uricer.edu.br>
2022-04-07 22:41:51 -0300

    Modificação no Controller de Atores

--- app/Http/Controllers/AtoresController.php
+++ app/Http/Controllers/AtoresController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ator;
 
 class AtoresController extends Controller
 {
     public function index () {
-        $nome = "Johnny Depp";
-        return view('atores', ['nome'=>$nome]);
+        $atores = Ator::All();
+        return view('atores.index', ['atores'=>$atores]);
+    }
+
+    public function create () {
+        return view('atores.create');
     }
 }
