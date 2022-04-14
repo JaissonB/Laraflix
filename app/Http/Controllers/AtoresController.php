7b6a0eea6af658bbbdefca49da2a40750b639112
JaissonB <097319@aluno.uricer.edu.br>
2022-04-14 20:16:25 -0300

    Adição das classes no controller

--- app/Http/Controllers/AtoresController.php
+++ app/Http/Controllers/AtoresController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AtorRequest;
 use App\Models\Ator;
 
 class AtoresController extends Controller
@@ -14,5 +15,27 @@ class AtoresController extends Controller
 
     public function create () {
         return view('atores.create');
+    }
+
+    public function store(AtorRequest $request) {
+        $novo_ator = $request->all();
+        Ator::create($novo_ator);
+
+        return redirect()->route('atores');
+    }
+
+    public function destroy($id) {
+        Ator::find($id)->delete();
+        return redirect()->route('atores');
+    }
+
+    public function edit($id) {
+        $ator = Ator::find($id);
+        return view('atores.edit', compact('ator'));
+    }
+
+    public function update(AtorRequest $request, $id) {
+        Ator::find($id)->update($request->all());
+        return redirect()->route('atores');
     }
 }
