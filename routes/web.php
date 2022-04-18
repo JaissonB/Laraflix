c95f55b505cbaff3564cf4e9b4f4fd8b67553658
JaissonB <097319@aluno.uricer.edu.br>
2022-04-18 19:16:20 -0300

    Adição de novas rotas

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/atores', [AtoresController::class, 'index']);
-Route::get('/atores/create', [AtoresController::class, 'create']);
+Route::group(['prefix'=>'atores', 'where'=>['id'=>'[0-9]+']], function () {
+    Route::get('',              ['as'=>'atores',         'uses'=>'App\Http\Controllers\AtoresController@index']);
+    Route::get('/create',       ['as'=>'atores.create',  'uses'=>'App\Http\Controllers\AtoresController@create']);
+    Route::get('/{id}/destroy', ['as'=>'atores.destroy', 'uses'=>'App\Http\Controllers\AtoresController@destroy']);
+    Route::get('/{id}/edit',    ['as'=>'atores.edit',    'uses'=>'App\Http\Controllers\AtoresController@edit']);
+    Route::put('/{id}/update',  ['as'=>'atores.update',  'uses'=>'App\Http\Controllers\AtoresController@update']);
+    Route::post('/store',       ['as'=>'atores.store',   'uses'=>'App\Http\Controllers\AtoresController@store']);
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
